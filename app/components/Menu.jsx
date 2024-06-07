f20015894e3d52757a08cfa4ecb089ebafab497a
mdsifaterabbi <mdsifaterabbi641@gmail.com>
2024-06-07 16:18:07 +0600

    second commit

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -5,12 +5,12 @@ const Menu = () => {
     <div className="bg-yellow-500 py-[50px]">
       <ul className="flex gap-2 justify-center">
         <li className="bg-purple-400 text-white px-[10px] py-[5px] hover:bg-orange-500 rounded-md">
-          <Link href={"http://localhost:3000/adminHome"} target="_blank">
+          <Link href={"https://ssr1-repo.onrender.com/adminHome"} target="_blank">
             adminHome
           </Link>
         </li>
         <li className="bg-purple-400 text-white px-[10px] py-[5px] hover:bg-orange-500 rounded-md">
-          <Link href={"http://localhost:3000/adminFAQ"} target="_blank">
+          <Link href={"https://ssr1-repo.onrender.com/adminFAQ"} target="_blank">
             adminFAQ
           </Link>
         </li>
